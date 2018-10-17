185d0c163ecf8891fcd0fa84417ba6f9a8aca323
charles <4charles2@users.noreply.github.com>
2018-10-17 19:21:52 +0200

    LCDScreen finich JLabel show create class actionListener begin

--- Calculatrice/src/matrix/Dashboard.java
+++ Calculatrice/src/matrix/Dashboard.java
@@ -1,11 +1,17 @@
 package matrix;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.AdjustmentListener;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import show.Background;
 import show.CreateButton;
@@ -13,14 +19,18 @@ import show.Fenetre;
 import show.PanelStaple;
 
 public class Dashboard{
+	static final String BEGIN_TEXT = "<html><H1 style='padding: 10px; font-size: 50px'>";
+	static final String AFTER_TEXT = "</H1></html>";
+	
 	PanelStaple LCDScreen = new PanelStaple();
 	Background back = new Background();
 	
 	public Dashboard() {
-		
+	
 		Fenetre show = new Fenetre("Maticalc", 500, 600);
 		
 		JPanel container = new JPanel();
+		LCDScreen.setSize(new Dimension(500, 100));
 		LCDScreen.setPreferredSize(new Dimension(500, 100));
 		
 		showButtons();
@@ -35,7 +45,15 @@ public class Dashboard{
 		show.validate();
 	}
 	private void showScreen() {
+		LCDScreen.setLayout(null);
 		
+		JLabel showLCD = new JLabel(BEGIN_TEXT+"0"+AFTER_TEXT);
+		showLCD.setBounds(0, 0, 490, 100);
+		showLCD.setVerticalAlignment(SwingConstants.CENTER);
+		showLCD.setHorizontalAlignment(SwingConstants.RIGHT);
+		showLCD.setBackground(Color.darkGray);
+		LCDScreen.add(showLCD);
+
 	}
 	private void showButtons() {
 		JPanel containNumber = new JPanel();
@@ -54,6 +72,7 @@ public class Dashboard{
 		numberButton.setOpaque(false);
 		operatorButton.setOpaque(false);
 		lastLigneNumber.setOpaque(false);
+		
 		
 		glOp.setHgap(10);
 		glOp.setVgap(10);
@@ -76,7 +95,7 @@ public class Dashboard{
 			if(temp.getText().matches("[1-9]"))
 				numberButton.add(temp);
 			else if( temp.getText() == "=") {
-				temp.setPreferredSize(new Dimension(210, 80));
+			temp.setPreferredSize(new Dimension(210, 80));
 				lastLigneNumber.add(temp);	
 			}else if(temp.getText().equals("0")) {
 				lastLigneNumber.add(temp);	
@@ -87,6 +106,13 @@ public class Dashboard{
 		containNumber.add(lastLigneNumber, BorderLayout.SOUTH);
 		stapleButtons.add(containNumber);
 		stapleButtons.add(operatorButton);
+		JButton resetButton = new JButton("C");
+		resetButton.setPreferredSize(new Dimension(420, 80));
+		back.add(resetButton);
 		back.add(stapleButtons);
+	}
+	
+	public class numberInput implements ActionListener{
+		
 	}
 }
