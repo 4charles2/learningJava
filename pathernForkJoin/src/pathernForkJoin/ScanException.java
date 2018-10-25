f6df37982d7354c72d0977249bc4dd5732180f23
charles tognol <ctognol@laposte.net>
2018-10-25 12:15:14 +0200

    Finish project parcourrirrepertoireSansFork with Thread and recursive

---
+++ pathernForkJoin/src/pathernForkJoin/ScanException.java
@@ -0,0 +1,11 @@
+package pathernForkJoin;
+
+public class ScanException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public ScanException(String arg0) {
+		super(arg0);
+	}
+
+}
